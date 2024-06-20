dc12bbaadaea56f9a5f471ebfe3f5c098d381407
mishail <mishail@HOME-PC>
2024-06-20 04:22:29 +0400

    -

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,13 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useEffect, useState } from 'react';
-import { FlatList, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { NavParamList } from '../types/NavProps';
 import Config from 'react-native-config';
 import { InstalledApps } from 'react-native-launcher-kit';
 
 type Props = NativeStackScreenProps<NavParamList, 'MainScreen'>;
 
-async function getData(setValue: React.Dispatch<React.SetStateAction<string>>, jwtKey: string)
+async function getData(setValue: React.Dispatch<React.SetStateAction<any>>, jwtKey: string)
 {
     const response = await fetch(Config.API + '/app/top?amount=10',
     {
@@ -16,18 +16,44 @@ async function getData(setValue: React.Dispatch<React.SetStateAction<string>>, j
     });
     const result = (await response.json());
     console.log(result);
+    setValue(result);
 }
 
 function MainScreen({ route, navigation }: Props) : React.JSX.Element
 {
     const { login, password, jwtKey } = route.params;
-    const [apps, setApps] = useState('');
-    useEffect(() => {getData(setApps, jwtKey)});
+    const [apps, setApps] = useState<any[]>([]);
+    useEffect(() => {getData(setApps, jwtKey)}, []);
     return(
-        <View>
-            <FlatList data={apps} renderItem={({item}) => {return(<View></View>)}}/>
+        <View style={styles.container}>
+            <FlatList style={styles.container} data={apps} renderItem={({item}) => { console.log(Config.API + '/files/apps/' + item.packageName + '/icon.png'); return(
+                <View style={styles.itemContainer}>
+                <Image height={70} width={70} source={{uri: Config.API + '/files/apps/' + item.packageName + '/icon.png'}}/>
+                <Text style={styles.textView}>{item.name}</Text>
+            </View>)}}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+    },
+    itemContainer: {
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    textView: {
+        backgroundColor: '#fff',
+        color: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        fontSize: 20,
+        marginLeft: 20
+    }
+});
 
 export default MainScreen;
